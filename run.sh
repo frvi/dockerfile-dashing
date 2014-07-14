52dc3340ac2a518d00f46ce4ec0279a6e326acf8
Fredrik Vihlborg <frvi@transmode.se>
2014-07-14 17:37:17 +0200

    Only install widgets and gems once.

--- run.sh
+++ run.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-# Install widgets
-if [[ ! -z "$WIDGETS" ]]; then
-  for WIDGET in $WIDGETS; do
-    echo -e "\nInstalling widget from gist $WIDGET"
-    dashing install "$WIDGET"
-  done
-fi
+function install_widgets() {
+  WIDGETS=$1
+  if [[ ! -z "$WIDGETS" ]]; then
+    for WIDGET in $WIDGETS; do
+      echo -e "\nInstalling widget from gist $WIDGET"
+      dashing install "$WIDGET"
+    done
+  fi
+}
 
-# Install gems (e g widget dependencies)
-if [[ ! -z "$GEMS" ]]; then
-  echo -e "\nInstalling gem(s) $GEMS"
-  echo "" >> Gemfile
-  for GEM in $GEMS; do
-    echo "gem '$GEM'" >> Gemfile
-  done
-  bundle install
+function install_gems() {
+  GEMS=$1
+  if [[ ! -z "$GEMS" ]]; then
+    echo -e "\nInstalling gem(s): $GEMS"
+    for GEM in $GEMS; do
+      echo -e "\ngem '$GEM'" >> Gemfile
+    done
+    bundle install
+  fi
+}
+
+if [[ ! -e /installed ]]; then
+  install_widgets $WIDGETS
+  install_gems $GEMS
+  touch /installed
 fi
 
 # Start dashing
